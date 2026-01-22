6cb4f62d5ac5f876df96f2567b956c3323ce63e7
Razria <dsyeom931008@gmail.com>
2026-01-22 19:14:21 +0900

    ✨ feat(binary-search-tree): implement BST Insert

--- CS_PracticeProject04/CS_PracticeProject04/BinarySearchTree.h
+++ CS_PracticeProject04/CS_PracticeProject04/BinarySearchTree.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdio>
+
 struct Node
 {
 public:
@@ -9,7 +11,7 @@ public:
 
 public:
 	Node() = default;
-	
+
 	Node(int InValue)
 		: Value(InValue)
 	{
@@ -24,4 +26,73 @@ public:
 
 public:
 	BinarySearchTree() = default;
+
+public:
+	bool Insert(int InValue)
+	{
+		Node* node = new Node;
+
+		node->Value = InValue;
+		node->Left = nullptr;
+		node->Right = nullptr;
+
+		if (!Root)
+		{
+			if (Size != 0)
+			{
+				std::printf("%-15s : %s\n", "Error", "InValid Root. but Size != 0.");
+				return false;
+			}
+
+			Root = node;
+			++Size;
+
+			std::printf("%-15s : NodePtr: %p | NodeValue: %-5d | Size: %zd\n", "Insert_Root", node, node->Value, Size);
+			return true;
+		}
+
+		// Valid Root
+		
+		Node* targetNode = Root;
+		while (targetNode != nullptr)
+		{
+			if (node->Value < targetNode->Value)
+			{
+				if (!targetNode->Left)
+				{
+					targetNode->Left = node;
+					++Size;
+
+					std::printf("%-15s : NodePtr: %p | NodeValue: %-5d | Size: %zd\n", "Insert_Left", node, node->Value, Size);
+					return true;
+				}
+				else // Valid targetNode->Left
+				{
+					targetNode = targetNode->Left;
+					continue;
+				}
+			}
+			else if(node->Value > targetNode->Value)
+			{
+				if (!targetNode->Right)
+				{
+					targetNode->Right = node;
+					++Size;
+
+					std::printf("%-15s : NodePtr: %p | NodeValue: %-5d | Size: %zd\n", "Insert_Right", node, node->Value, Size);
+					return true;
+				}
+				else // Valid targetNode->Left
+				{
+					targetNode = targetNode->Right;
+					continue;
+				}
+			}
+			else // node->Value == targetNode->Value
+			{
+				std::printf("%-15s : %s\n", "Error", "InValue be Duplicated.");
+				return false;
+			}
+		}
+	}
 };
--- CS_PracticeProject04/CS_PracticeProject04/main.cpp
+++ CS_PracticeProject04/CS_PracticeProject04/main.cpp
@@ -4,5 +4,12 @@
 
 int main()
 {
+	std::printf("\n[BST Init]\n");
+	BinarySearchTree bst;
+	bst.Insert(100);
+	bst.Insert(50);
+	bst.Insert(200);
+	bst.Insert(100);
+
 	return 0;
 }
