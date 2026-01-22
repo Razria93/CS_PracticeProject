e3fcca67a0c62a48389374df3c5e28a320ac4230
Razria <dsyeom931008@gmail.com>
2026-01-22 18:28:36 +0900

    ✨ feat(binary-search-tree): implement Node structure

--- CS_PracticeProject04/CS_PracticeProject04/BinarySearchTree.h
+++ CS_PracticeProject04/CS_PracticeProject04/BinarySearchTree.h
@@ -1,1 +1,17 @@
 #pragma once
+
+struct Node
+{
+public:
+	int Value;
+	Node* Left = nullptr;
+	Node* Right = nullptr;
+
+public:
+	Node() = default;
+	
+	Node(int InValue)
+		: Value(InValue)
+	{
+	}
+};
