720e22bb432a87f54934f86c7e2f36b08bfa015e
Razria <dsyeom931008@gmail.com>
2026-01-30 21:07:30 +0900

    ✨ feat(hash-table): add initial Node/Bucket structures and HashTable initialization

--- CS_PracticeProject06/CS_PracticeProject06/HashTable.h
+++ CS_PracticeProject06/CS_PracticeProject06/HashTable.h
@@ -1,1 +1,66 @@
 #pragma once
+
+struct Node
+{
+	int Value;
+	Node* PrevNode;
+	Node* NextNode;
+
+public:
+	Node()
+	{
+		Value = 0;
+		PrevNode = nullptr;
+		NextNode = nullptr;
+	}
+
+	~Node()
+	{
+		Value = 0;
+		PrevNode = nullptr;
+		NextNode = nullptr;
+	}
+};
+
+struct Bucket
+{
+	Node* BaseNode;
+	size_t Size;
+
+public:
+	Bucket(size_t InSize)
+	{
+		BaseNode = new Node[InSize];
+		Size = InSize;
+	}
+
+	~Bucket()
+	{
+		delete[] BaseNode;
+		Size = 0;
+	}
+};
+
+class HashTable
+{
+public:
+	Bucket* BaseBucket; // == std::vector<Node>(InSize) && Chainning
+
+public:
+	HashTable()
+	{
+		BaseBucket = nullptr;
+	}
+
+	~HashTable()
+	{
+		delete BaseBucket;
+	}
+
+public:
+	void Initialize(size_t InSize)
+	{
+		if (BaseBucket) return;
+		BaseBucket = new Bucket(InSize);
+	}
+};
--- CS_PracticeProject06/CS_PracticeProject06/main.cpp
+++ CS_PracticeProject06/CS_PracticeProject06/main.cpp
@@ -3,5 +3,8 @@
 
 int main()
 {
+	HashTable* hashTable = new HashTable;
+	hashTable->Initialize(10);
+	
 	return 0;
 }
