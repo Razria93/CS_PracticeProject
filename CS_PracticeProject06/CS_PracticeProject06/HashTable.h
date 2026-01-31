9df305368a7d6c7c1ecce039d5c3ab8e31f29f34
Razria <dsyeom931008@gmail.com>
2026-01-31 16:16:10 +0900

    ✨ feat(hash-table): implement HashTable and Bucket Insert

--- CS_PracticeProject06/CS_PracticeProject06/HashTable.h
+++ CS_PracticeProject06/CS_PracticeProject06/HashTable.h
@@ -50,6 +50,78 @@ public:
 		Capacity = 0;
 		Size = 0;
 	}
+
+public:
+	bool Insert(int InValue)
+	{
+		// [Policy] No duplicates allowed (Set)
+
+		if (!BaseNode)
+		{
+			printf("[%s/%s] %s\n", "Error", "Insert", "BaseBucket is invalid.");
+			return false;
+		}
+
+		size_t key = static_cast<size_t>(InValue);
+		size_t hashkey = HashFunction(key);
+
+		Node* targetNodeInBucket = BaseNode + hashkey;
+
+		/* Case01: targetNodeInBucket->bExist == false */
+		if (!targetNodeInBucket->bExist)
+		{
+			targetNodeInBucket->bExist = true;
+			targetNodeInBucket->HashKey = hashkey;
+			targetNodeInBucket->Value = InValue;
+
+			++Size;
+
+			printf("[%s/%s] %s: %d | %s: %zd\n", "Complete", "Insert", "Insert complete", InValue, "Size", Size);
+			return true;
+		}
+
+		/* Case02: targetNodeInBucket->bExist == true */
+		Node* targetNode = targetNodeInBucket;
+		while (true)
+		{
+			if (targetNode->HashKey != hashkey)
+			{
+				printf("[%s/%s] %s\n", "Error", "Insert", "HashKey mismatch.");
+				return false;
+			}
+
+			if (targetNode->Value == InValue)
+			{
+				printf("[%s/%s] %s\n", "Failed", "Insert", "Duplicate entries are not allowed.");
+				return false;
+			}
+
+			if (!targetNode->NextNode)
+			{
+				Node* newNode = new Node();
+
+				newNode->bExist = true;
+				newNode->HashKey = hashkey;
+				newNode->Value = InValue;
+
+				newNode->PrevNode = targetNode;
+				targetNode->NextNode = newNode;
+
+				++Size;
+
+				printf("[%s/%s] %s: %d | %s: %zd\n", "Complete", "Insert", "Insert complete", InValue, "Size", Size);
+				return true;
+			}
+			else
+			{
+				targetNode = targetNode->NextNode;
+				continue;
+			}
+		}
+
+		printf("[%s/%s] %s\n", "Failed", "Insert", "Undefined");
+		return false;
+	}
 };
 
 class HashTable
@@ -79,5 +151,17 @@ public:
 
 		BaseBucket = new Bucket(InSize);
 		printf("[%s/%s] %s\n", "Complete", "Initialize", "Initialize complete.");
+	}
+
+public:
+	bool Insert(int InValue)
+	{
+		if (!BaseBucket)
+		{
+			printf("[%s/%s] %s\n", "Error", "Insert", "BaseBucket is InValid.");
+			return false;
+		}
+
+		return BaseBucket->Insert(InValue);
 	}
 };
