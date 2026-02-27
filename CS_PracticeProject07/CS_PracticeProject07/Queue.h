67568f53091392549d8427efd4327d36e8ff78f3
Razria <dsyeom931008@gmail.com>
2026-02-27 14:04:47 +0900

    ✨ feat(queue): implement Queue push

--- CS_PracticeProject07/CS_PracticeProject07/Queue.h
+++ CS_PracticeProject07/CS_PracticeProject07/Queue.h
@@ -11,22 +11,69 @@ private:
 	size_t Size = 0;
 
 public:
-	Queue() = default;
+	Queue(int InSize)
+	{
+		Base = new int[InSize] {};
+		Size = 0;
+		Capacity = InSize;
+	}
+
+public:
+	~Queue()
+	{
+		delete[] Base;
+		Size = 0;
+		Capacity = 0;
+	}
 
 public:
 	// State
 	bool empty() { return Size == 0; }
 	bool full() { return Size == Capacity; }
-	
+
 	// Size
-	int size() { return Size; }
-	
+	size_t size() { return Size; }
+
 	// Iterator
 	int* front() { return Base + Front; }
 	int* back() { return Base + Rear; }
 
 public:
-	void push(int InValue) {};
+	void push(int InValue)
+	{
+		if (full()) 
+		{
+			std::cout << "[NOTE] push(" << InValue << ")" << " is invalid." << '\n';
+			return;
+		}
+
+		Base[Rear] = InValue;
+		Rear = (Rear + 1) % Capacity;
+		++Size;
+	};
+
 	void pop() {};
 	void clear() {};
+
+public:
+	void print()
+	{
+		if (empty())
+			std::cout << "[NOTE] Queue is empty." << '\n';
+		
+		std::cout << '\n';
+		std::cout << "=== Queue Print ===" << '\n';
+		std::cout << "[Base Info]" << '\n';
+		std::cout << "- Capacity :" << Capacity << '\n';
+		std::cout << "- Size :" << Size << '\n';
+		std::cout << "- Front_idx :" << Front << '\n';
+		std::cout << "- Rear_idx :" << Rear << '\n';
+
+		std::cout << '\n';
+		std::cout << "[Idx and Value]" << '\n';
+		for (int i = 0; i < Capacity; ++i)
+		{
+			std::cout << "idx_" << i << ": " << Base[i] << '\n';
+		}
+	}
 };
--- CS_PracticeProject07/CS_PracticeProject07/main.cpp
+++ CS_PracticeProject07/CS_PracticeProject07/main.cpp
@@ -5,5 +5,19 @@ int main()
 {
 	Queue* queue = new Queue(10);
 
+	std::cout << "[Push Test]" << '\n';
+	queue->push(1);
+	queue->push(2);
+	queue->push(3);
+	queue->push(4);
+	queue->push(5);
+	queue->push(6);
+	queue->push(7);
+	queue->push(8);
+	queue->push(9);
+	queue->push(10);
+	queue->push(11);
+	queue->print();
+
 	return 0;
 }
