36888111f8e789ce51b88b7f61190fbc79708528
Razria <dsyeom931008@gmail.com>
2026-02-27 18:23:05 +0900

    ✨ feat(heap): implement MinHeap push

--- CS_PracticeProject08/CS_PracticeProject08/Heap.h
+++ CS_PracticeProject08/CS_PracticeProject08/Heap.h
@@ -30,7 +30,59 @@ public:
 	int top() { return Base[0]; }
 
 public:
-	void push(int InValue) {};
+	void push(int InValue)
+	{
+		if (full())
+		{
+			std::cout << "[NOTE] push(" << InValue << ")" << " is invalid." << '\n';
+			return;
+		}
+
+		Base[Size] = InValue;
+		++Size;
+
+		int invalue_idx = Size - 1;
+		int parent_idx = idx_parent(invalue_idx);
+
+		while (invalue_idx != parent_idx && InValue < Base[parent_idx])
+		{
+			Base[invalue_idx] = Base[parent_idx];
+			Base[parent_idx] = InValue;
+
+			invalue_idx = parent_idx;
+			parent_idx = idx_parent(invalue_idx);
+		}
+	};
+
 	void pop() {};
 	void clear() {};
+
+private:
+	int idx_parent(int InValue) { return (InValue - 1) / 2; }
+	int idx_L_child(int InValue) { return InValue * 2 + 1; }
+	int idx_R_child(int InValue) { return InValue * 2 + 2; }
+
+public:
+	void print()
+	{
+		std::cout << "=== Queue Print ===" << '\n';
+		std::cout << "[Base Info]" << '\n';
+		std::cout << "- Capacity :" << Capacity << '\n';
+		std::cout << "- Size :" << Size << '\n';
+
+		std::cout << '\n';
+		std::cout << "[Idx and Value]" << '\n';
+
+		if (empty())
+			std::cout << "[NOTE] Queue is empty." << '\n';
+		else
+		{
+			for (int i = 0; i < Capacity; ++i)
+			{
+				std::cout << "idx_" << i << ": " << Base[i] << '\n';
+			}
+		}
+
+		std::cout << "===================" << '\n';
+	}
 };
--- CS_PracticeProject08/CS_PracticeProject08/main.cpp
+++ CS_PracticeProject08/CS_PracticeProject08/main.cpp
@@ -4,6 +4,40 @@
 int main()
 {
 	MinHeap* minHeap = new MinHeap(10);
+	minHeap->push(10);
+	minHeap->print();
+
+	minHeap->push(9);
+	minHeap->print();
+
+	minHeap->push(8);
+	minHeap->print();
+
+	minHeap->push(7);
+	minHeap->print();
+	
+	minHeap->push(6);
+	minHeap->print();
+	
+	minHeap->push(5);
+	minHeap->print();
+	
+	minHeap->push(4);
+	minHeap->print();
+	
+	minHeap->push(3);
+	minHeap->print();
+	
+	minHeap->push(2);
+	minHeap->print();
+	
+	minHeap->push(1);
+	minHeap->print();
+	
+	minHeap->push(0);
+	minHeap->print();
+
+	std::cout << "Top is " << minHeap->top();
 
 	return 0;
 }
